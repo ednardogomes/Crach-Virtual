102d88085c41d1e39481f4ddc0169b4c7ad6bbf5
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-04-08 17:32:16 -0300

    início do projeto crachá virtual

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const linksSocialMedia = {
+  github: "ednardogomes",
+  youtube: "UCzbNO1D90cljPa6TfhnhF5Q",
+  facebook: "ednardo.gomes.37",
+  instagram: "ednardo94/",
+  twitter: "EdnardoGomesDev",
+};
+
+function changeSocialMediaLinks() {
+  document.getElementById("userName").textContent = "Olivia";
+}
+changeSocialMediaLinks();
